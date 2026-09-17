48e9bd02b14afeb222d888fe865d2cda93e97974
Makoto Fujiwara <makoto@if.t.u-tokyo.ac.jp>
2026-09-18 00:22:22 +0900

    space around ->

--- GDSaref.c
+++ GDSaref.c
@@ -40,16 +40,16 @@ GDSgetArefBBox(arefEl *aref, transform *transf)
   bbx.ur.x = -BIGVAL;
   bbx.ur.y = -BIGVAL;
 
-  for(i = 0; i < aref->cols; i++)
-    for(j = 0; j < aref->rows; j++)
+  for(i = 0; i < aref -> cols; i++)
+    for(j = 0; j < aref -> rows; j++)
     {
-      sref.strptr = aref->strptr;
-      sref.refname = aref->refname;
+      sref.strptr = aref -> strptr;
+      sref.refname = aref -> refname;
 
-      refpoint.x = i * aref->colspacing;
-      refpoint.y = j * aref->rowspacing;
+      refpoint.x = i * aref -> colspacing;
+      refpoint.y = j * aref -> rowspacing;
       sref.transfptr = GDStranslateTransf(&Ident, refpoint);
-      newtransf = GDStransfTransf(aref->transfptr, transf);
+      newtransf = GDStransfTransf(aref -> transfptr, transf);
       bbx1 = GDSgetSrefBBox(&sref, newtransf);
 
       GDSfreeTransf(sref.transfptr);
@@ -75,16 +75,16 @@ ArefToPS(FILE *psfile, arefEl *aref, transform *transf, PSStyle psStyle)
   point refpoint;
   int i, j;
 
-  for(i = 0; i < aref->cols; i++)
-    for(j = 0; j < aref->rows; j++)
+  for(i = 0; i < aref -> cols; i++)
+    for(j = 0; j < aref -> rows; j++)
     {
-      sref.strptr = aref->strptr;
-      sref.refname = aref->refname;
+      sref.strptr = aref -> strptr;
+      sref.refname = aref -> refname;
 
-      refpoint.x = i * aref->colspacing;
-      refpoint.y = j * aref->rowspacing;
+      refpoint.x = i * aref -> colspacing;
+      refpoint.y = j * aref -> rowspacing;
       sref.transfptr = GDStranslateTransf(&Ident, refpoint);
-      newtransf = GDStransfTransf(aref->transfptr, transf);
+      newtransf      = GDStransfTransf(aref -> transfptr, transf);
       SrefToPS(psfile, &sref, newtransf, psStyle);
       GDSfreeTransf(sref.transfptr);
       GDSfreeTransf(newtransf);
@@ -98,20 +98,20 @@ ArefToPOV(FILE *povfile, arefEl *aref, transform *transf, PSStyle psStyle)
   transform *newtransf;
   point refpoint;
   int i, j;
-  fprintf(stderr,"Rendering Aref->POV\n"
+  fprintf(stderr,"Rendering Aref -> POV\n"
     "  Rows: %d\n  Cols: %d\n  RowSpace: %d\n  ColSpace: %d\n",
-    aref->rows,aref->cols,aref->rowspacing,aref->colspacing
+    aref -> rows,aref -> cols,aref -> rowspacing,aref -> colspacing
    ); 
-  for(i = 0; i < aref->cols; i++)
-    for(j = 0; j < aref->rows; j++)
+  for(i = 0; i < aref -> cols; i++)
+    for(j = 0; j < aref -> rows; j++)
     {
-      sref.strptr = aref->strptr;
-      sref.refname = aref->refname;
+      sref.strptr = aref -> strptr;
+      sref.refname = aref -> refname;
 
-      refpoint.x = i * aref->colspacing;
-      refpoint.y = j * aref->rowspacing;
+      refpoint.x = i * aref -> colspacing;
+      refpoint.y = j * aref -> rowspacing;
       sref.transfptr = GDStranslateTransf(&Ident, refpoint);
-      newtransf = GDStransfTransf(aref->transfptr, transf);
+      newtransf = GDStransfTransf(aref -> transfptr, transf);
       /* Here we have a sref being instanced with additional
                translations. */
       SrefToPOV(povfile, &sref, newtransf, psStyle);
@@ -128,16 +128,16 @@ ArefToHPGL(FILE *hpglfile, arefEl *aref, transform *transf, PSStyle psStyle)
   point refpoint;
   int i, j;
   
-  for(i = 0; i < aref->cols; i++)
-    for(j = 0; j < aref->rows; j++)
+  for(i = 0; i < aref -> cols; i++)
+    for(j = 0; j < aref -> rows; j++)
     {
-      sref.strptr = aref->strptr;
-      sref.refname = aref->refname;
+      sref.strptr = aref -> strptr;
+      sref.refname = aref -> refname;
 
-      refpoint.x = i * aref->colspacing;
-      refpoint.y = j * aref->rowspacing;
+      refpoint.x = i * aref -> colspacing;
+      refpoint.y = j * aref -> rowspacing;
       tp1 = GDStranslateTransf(&Ident, refpoint);
-      transfptr = GDStransfTransf(tp1, aref->transfptr);
+      transfptr = GDStransfTransf(tp1, aref -> transfptr);
       sref.transfptr = GDStransfTransf(transfptr, transf);
       SrefToHPGL(hpglfile, &sref, sref.transfptr, psStyle);
       GDSfreeTransf(sref.transfptr);
@@ -158,16 +158,16 @@ GDSreadAref(int gdsfildes, GDSstruct *structptr)
   arefEl *arefptr;
 
   arefptr = (arefEl *)MALLOC(sizeof(arefEl));
-  arefptr->strptr = NULL; /* to be inserted after all structs have been read */
+  arefptr -> strptr = NULL; /* to be inserted after all structs have been read */
   newcell = (GDScell *)MALLOC(sizeof(GDScell));
-  newcell->type = AREF;
-  newcell->detail.aref = arefptr;
+  newcell -> type = AREF;
+  newcell -> detail.aref = arefptr;
   if(GDSreadRecord(gdsfildes, &record, &nbytes) != SNAME)
   {
     fprintf(stderr, "Missing SNAME field in AREF element. Abort!\n");
     /* exit(1); */
   }
-  if((arefptr->refname = GDSreadString(record + 2, nbytes - 4)) == NULL)
+  if((arefptr -> refname = GDSreadString(record + 2, nbytes - 4)) == NULL)
   {
     fprintf(stderr, "Bad STRING field in AREF element. Abort!\n");
     exit(1);
@@ -201,14 +201,14 @@ GDSreadAref(int gdsfildes, GDSstruct *structptr)
               fprintf(stderr, "Missing COLROW field in AREF element. Abort!\n");
               exit(1);
             }
-            arefptr->cols = GDSreadInt2(record + 2);
-            arefptr->rows = GDSreadInt2(record + 4);
+            arefptr -> cols = GDSreadInt2(record + 2);
+            arefptr -> rows = GDSreadInt2(record + 4);
             FREE(record);
             break;
           case COLROW:
             angle = CURRENTANGLE;
-            arefptr->cols = GDSreadInt2(record + 2);
-            arefptr->rows = GDSreadInt2(record + 4);
+            arefptr -> cols = GDSreadInt2(record + 2);
+            arefptr -> rows = GDSreadInt2(record + 4);
             FREE(record);
             break;
           default:
@@ -227,15 +227,15 @@ GDSreadAref(int gdsfildes, GDSstruct *structptr)
           fprintf(stderr, "Missing COLROW field in AREF element. Abort!\n");
           exit(1);
         }
-        arefptr->cols = GDSreadInt2(record + 2);
-        arefptr->rows = GDSreadInt2(record + 4);
+        arefptr -> cols = GDSreadInt2(record + 2);
+        arefptr -> rows = GDSreadInt2(record + 4);
         FREE(record);
         break;
       case COLROW:
         mag = CURRENTMAG;
         angle = CURRENTANGLE;
-        arefptr->cols = GDSreadInt2(record + 2);
-        arefptr->rows = GDSreadInt2(record + 4);
+        arefptr -> cols = GDSreadInt2(record + 2);
+        arefptr -> rows = GDSreadInt2(record + 4);
         FREE(record);
         break;
       default:
@@ -248,8 +248,8 @@ GDSreadAref(int gdsfildes, GDSstruct *structptr)
   {
     mag = CURRENTMAG;
     angle = CURRENTANGLE;
-    arefptr->cols = GDSreadInt2(record + 2);
-    arefptr->rows = GDSreadInt2(record + 4);
+    arefptr -> cols = GDSreadInt2(record + 2);
+    arefptr -> rows = GDSreadInt2(record + 4);
     FREE(record);
   }
   else
@@ -263,7 +263,7 @@ GDSreadAref(int gdsfildes, GDSstruct *structptr)
     fprintf(stderr, "Missing XY field in AREF element. Abort!\n");
     exit(1);
   }
-  if(arefptr->rows == 0 || arefptr->cols == 0)
+  if(arefptr -> rows == 0 || arefptr -> cols == 0)
   {
     fprintf(stderr, "AREF element with 0 rows/cols.  Abort!\n");
     exit(1);
@@ -277,43 +277,43 @@ GDSreadAref(int gdsfildes, GDSstruct *structptr)
   row.y = GDSreadInt4(record + 22);
   FREE(record);
 
-  arefptr->transfptr = GDSgetTransf(ref, angle, mag, mirror);
+  arefptr -> transfptr = GDSgetTransf(ref, angle, mag, mirror);
 
 /* Since GDSinvtransfPoint performs also the translation, the unused dimension
    must be equal to 0 (i.e. the original one was identical to the reference
    point one) and the useful one is exactly the spacing multiplied by the number
    of cells in that direction */
 
-  col = GDSinvtransfPoint(&col, arefptr->transfptr);
+  col = GDSinvtransfPoint(&col, arefptr -> transfptr);
 
   if(col.x < 0)
   {
-    fprintf(stdout, " %04d Error in AREF (%s) Found a y-axis mirrored array. This is impossible so I'm exiting.\n", __LINE__, arefptr->transfptr);
+    fprintf(stdout, " %04d Error in AREF (%s) Found a y-axis mirrored array. This is impossible so I'm exiting.\n", __LINE__, arefptr -> transfptr);
     exit(1);    
   }
   if(col.y != 0)
   {
     fprintf(stdout, " %04d Error in AREF (%s) The second point in XY %3.2f (%d) is broken.\n",
-	    __LINE__, arefptr-> refname, col.y, col.y );
+	    __LINE__, arefptr ->  refname, col.y, col.y );
   }
 
-  row = GDSinvtransfPoint(&row, arefptr->transfptr);
+  row = GDSinvtransfPoint(&row, arefptr -> transfptr);
 
   if(row.x != 0)
   {
     fprintf(stdout, " %04d Error in AREF (%s) The third point in XY is broken.\n",
-	    __LINE__, arefptr-> refname);
+	    __LINE__, arefptr ->  refname);
     exit(1);
   }
 
-  arefptr->colspacing =
-    col.x / arefptr->cols;
-  arefptr->rowspacing =
-    row.y / arefptr->rows;
+  arefptr -> colspacing =
+    col.x / arefptr -> cols;
+  arefptr -> rowspacing =
+    row.y / arefptr -> rows;
 
   if(row.y < 0)
-    arefptr->rowspacing *=-1.0;
-/*    arefptr->transfptr->c -= arefptr->rowspacing; */
+    arefptr -> rowspacing *=-1.0;
+/*    arefptr -> transfptr -> c -= arefptr -> rowspacing; */
   if(GDSreadRecord(gdsfildes, &record, &nbytes) != ENDEL)
   {
     fprintf(stderr, "Missing ENDEL field in AREF element. Skipping!\n");
@@ -323,7 +323,7 @@ GDSreadAref(int gdsfildes, GDSstruct *structptr)
 
   fprintf(stdout, " %04d %s Aref at %9d,%9d of cell named \"%s\", c/r  = %d/%d, spacing = %d/%d, mirror = %d\n", 
 	  __LINE__, __func__,
-          ref.x, ref.y, arefptr->refname, arefptr->cols, arefptr->rows,
-          arefptr->colspacing, arefptr->rowspacing, mirror);  
+          ref.x, ref.y, arefptr -> refname, arefptr -> cols, arefptr -> rows,
+          arefptr -> colspacing, arefptr -> rowspacing, mirror);  
   return newcell;
 }
