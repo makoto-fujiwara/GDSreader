adca999978ca8eddeb95574864c9811ac1d9870b
Makoto Fujiwara <makoto@if.t.u-tokyo.ac.jp>
2026-09-22 11:16:02 +0900

    (GDSglobal.h) add newfile, but should consider rename ?

---
+++ GDSglobal.h
@@ -0,0 +1,10 @@
+/*
+ * gdsreader - simple Calma parser/printer tool
+ */
+#ifndef _GDSglobal_h_
+#define _GDSglobal_h_
+
+extern int  use_userunit;
+extern int  show_summary;
+
+#endif /* _GDSglobal.h_ */
